925bc3c1bab71d1936a73c79051284e1a80670ee
Chris <chrisnicole1104@gmail.com>
2025-05-20 12:32:15 +0200

    Ultime modifiche relative ad alcuni test

--- tests/countCharacters.test.js
+++ tests/countCharacters.test.js
@@ -3,4 +3,7 @@ const op = require("../src/stringUtils");
 test("Stringa 'lollo' ", () => {
     expect(op.countCharacters("lollo")).toEqual( {l: 3, o: 2} );
 });
+test("Stringa ' ' ", () => {
+    expect(op.countCharacters(" ")).toEqual( { " " : 1} );
+});
 
--- tests/truncateString.test.js
+++ tests/truncateString.test.js
@@ -8,4 +8,12 @@ test("Stringa 'Ciao Frenk' troncata a partire dalla posizione 2: Mi...`", () => 
     expect(op.truncateString("Ciao Frenk",10)).toBe("Ciao Frenk");
 });
 
+test("Stringa 'F' troncata a partire dalla posizione 2: Mi...`", () => {
+    expect(op.truncateString("F",21)).toBe("F");
+});
+
+test("Stringa 'F' troncata a partire dalla posizione 2: Mi...`", () => {
+    expect(op.truncateString("F",-1)).toBe("...");
+});
+
 
